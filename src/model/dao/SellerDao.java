66e535924b48e9175fb344a84436e614105f6367
Leandor Oliveira <leandro_l.s.o@hotmail.com>
2023-06-12 21:04:18 -0300

    findByDepartment implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,6 +1,8 @@
 package model.dao;
 
 import java.util.List;
+
+import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
@@ -8,6 +10,7 @@ public interface SellerDao {
 	void insert (Seller obj); // inserir no banco de dados um Seller a partir do parâmetro recebido obj
 	void update(Seller obj); // atualiza
 	void deleteById(Integer id); // exlcui do banco de dados a partir do recebimento de um ID
-	Seller findById(Integer id); // pega o ID e consulta no banco de dados
-	List<Seller> findAll(); // listar todos os departamentos
+	Seller findById(Integer id); // consultar o vendedor por ID no banco de dados
+	List<Seller> findAll(); // listar todos os os vendedores 
+	List<Seller> findByDepartment(Department department);// Listar os vendedores por ID do Departamento
 }
