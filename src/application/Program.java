e7611af4c9f771f75e9c8a56238b9716779e55bf
Leandor Oliveira <leandro_l.s.o@hotmail.com>
2023-06-15 18:44:50 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
-import java.util.Scanner; 
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -31,6 +33,10 @@ public class Program {
 			System.out.println(obj); //Para cada Seller obj da lista list, imprima na tela o obj.
 		}
 		
+		System.out.println("\n=== Test 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New ID = " + newSeller.getId());
 		sc.close();
 	}
 
